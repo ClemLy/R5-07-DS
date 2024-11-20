b64e038a76f23d0458fa6b6a2dc393186d531ac9
ClemLy <119584744+ClemLy@users.noreply.github.com>
2024-11-20 14:12:34 +0100

    Create Q4.java

---
+++ Q4.java
@@ -0,0 +1,34 @@
+public class Q4
+{
+    public static void main(String[] args)
+    {
+        if (args.length < 1)
+        {
+            System.out.println("Veuillez fournir un nombre en paramètre.");
+            return;
+        }
+
+        try
+        {
+            int n = Integer.parseInt(args[0]);
+
+            if (n > 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    System.out.println("*".repeat(i));
+                }
+            }
+
+            else
+            {
+                System.out.println("Veuillez entrer un nombre positif.");
+            }
+        }
+        
+        catch (NumberFormatException e)
+        {
+            System.out.println("Le paramètre fourni doit être un entier.");
+        }
+    }
+}
